2dec79784149bbf11902d3c53409b9ba1f69a18b
ThatXliner <bryan.hu.2020@gmail.com>
2023-09-22 14:41:18 -0700

    Added comment as suggested

--- src/main/java/frc/robot/led/LEDConstants.java
+++ src/main/java/frc/robot/led/LEDConstants.java
@@ -16,5 +16,6 @@ public class LEDConstants {
   public static final Color kLockSwerve = Color.green;
   public static final Color kSuccess = Color.green;
   public static final Color kError = Color.red;
+  // A nice shade of green
   public static final Color kDefault = new Color(0.3803921569f, 1.0f, 0.5450980392f);
 }
